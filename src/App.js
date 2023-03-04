7d9cf2559fd21bb313773fae320db7a686d52215
goldhaxx <zwright7@gmail.com>
2023-03-04 15:51:33 -0800

    large edits to support nav & button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Button from "@mui/material/Button";
+import MenuAppBar from './Appbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MenuAppBar/>
+      <Button variant="contained">Hello World</Button>
     </div>
-  );
+  )
+  ;
 }
 
 export default App;
